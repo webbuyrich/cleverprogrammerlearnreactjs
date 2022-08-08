24ffb75949de51b9b82efd3091e015176dfc23c7
Richard Peterson <webbuyrich@gmail.com>
2022-08-08 17:24:18 -0500

    added comments, getElementById

---
+++ jscrashcourse/home.js
@@ -0,0 +1,21 @@
+console.log('hello');
+
+
+//alert('Hello this is Richard');
+
+// How to run a comment inline
+// alert('Hello this is Richard');
+
+
+// Variables
+var b = 'smoothie';
+console.log(b);
+
+var someNumber = 45;
+console.log(someNumber);
+
+// add html code to JS page. This will add text to the corresponding element with an ID of someText
+document.getElementById('someText').innerHTML = 'Hey there';
+
+// add html code to JS page. This will add text to the corresponding element with an ID of thisText
+document.getElementById('thisText').innerHTML = 'Sample Heading 1';
