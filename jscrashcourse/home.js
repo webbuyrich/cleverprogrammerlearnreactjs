370f78c09616b1536127e7f37a8ee10529e5c3bf
Richard Peterson <webbuyrich@gmail.com>
2022-08-10 15:39:54 -0500

    js strings practice

--- jscrashcourse/home.js
+++ jscrashcourse/home.js
@@ -1,76 +1,15 @@
-console.log('hello');
-
-
-//alert('Hello this is Richard');
-
-// How to run a comment inline
-// alert('Hello this is Richard');
-
-
-// Variables
-var b = 'smoothie';
-console.log(b);
-
-var someNumber = 45;
-console.log(someNumber);
-
-// add html code to JS page. This will add text to the corresponding element with an ID of someText
-document.getElementById('someText').innerHTML = 'Hey there';
-
-// add html code to JS page. This will add text to the corresponding element with an ID of thisText
-document.getElementById('thisText').innerHTML = 'Sample Heading 1';
-
-// ALLOWING AGE TO BE STORED
-// var age = prompt('What is your age?');
-// // add age variable to other element
-// document.getElementById('ageText').innerHTML = 'My age is ' + age + ' and you better believe it!';
-
-// NUMBERS IN JAVASCRIPT
-var num1 = 20;
-// increase number by 1
-num1 = num1 + 1;
-num1++;
-console.log(num1);
-
-//decrease number by 1
-num1--;
-console.log(num1);
-
-console.log(2 *40);
-
-/**--------------------------------------- */
-
-/*
-FUNCTIONS
-    1. Create Function
-    2. Call the Function
-*/
-
-// Create Function
-function fun (){
-    alert('this is a function');
-}
-// Call the function
-fun();
 
 
 
-// create function that takes a name and returns hello followed by your name
-/**
- * For example
- * Name: "Richard"
- * Return: "Hello Richard"
- */
+/* STRINGS */
 
- function greeting(){
-    // ask and store a person's name
-    var name = prompt('What is your name');
-    // add name with greeting
-    var result = 'Hello ' + name;
-    // display result
-    console.log(result);
+let fruit = 'banana';
+let moreFruits = 'banana\napple';           //new line
 
-}
-// Call the function
-greeting()
-
+console.log(fruit.length);
+console.log(fruit.indexOf('an'));           // returns 1
+console.log(fruit.indexOf('q'));            // returns -1 not in string
+console.log(fruit.indexOf('nan'));          // finds at 2
+console.log(fruit.slice(2, 3));             // returns n
+console.log(fruit.replace('ban', '123'));   // returns 123ana
+console.log(fruit.toUpperCase()); 
