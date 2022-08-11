fcefd14cc7a686df199a36736fc2c3f0846b730d
Richard Peterson <webbuyrich@gmail.com>
2022-08-11 14:46:51 -0500

    added logic to loop through guesses array and display li item

--- jscrashcourse/numberexercise/script.js
+++ jscrashcourse/numberexercise/script.js
@@ -7,7 +7,7 @@
  * DONE: Complete the showYouWon, showNumberAbove, showNumberBelow
  * DONE: Use the showYouWon... functions within displayResult to display the correct dialog
  * DONE: Save the guess history in a variable called guess
- * TODO: Display the guess history using displayHistory() function
+ * DONE: Display the guess history using displayHistory() function
  * TODO: Use the initGame() function to restart the game
  */
 
@@ -33,7 +33,7 @@ function playGame(){
   let numberGuess = document.getElementById('number-guess').value;
   saveGuessHistory(numberGuess);
   displayResult(numberGuess);
-  
+  displayHistory();
 }
 
 
@@ -100,9 +100,13 @@ function saveGuessHistory(guess) {
  * HINT: use while loop and string concatentation to create a list of guesses
  */
 function displayHistory() {
-  let index; // TODO
+  let index = 0; // TODO
   let list = "<ul class='list-group'>";
   // *CODE GOES BELOW HERE *
+  while( index < guesses.length){
+    list +="<li class='list-group-item'>You guessed " + guesses[index] + "</li>";
+    index += 1;
+  }
   list += '</ul>'
   document.getElementById("history").innerHTML = list;
 }
