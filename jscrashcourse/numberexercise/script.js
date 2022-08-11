b8207f125afc153b2ff6ee527b04b51fb4157d82
Richard Peterson <webbuyrich@gmail.com>
2022-08-11 14:32:18 -0500

    create empty array to hold guesses. added push to guesses array for each guess. add function to saveGuessHistory

--- jscrashcourse/numberexercise/script.js
+++ jscrashcourse/numberexercise/script.js
@@ -5,13 +5,14 @@
  * DONE: Console whether the guess is too high, too low, or is correct inside playGame function
  * DONE: Create a function called displayResult to move the logic for if the guess is too high, too low, or correct
  * DONE: Complete the showYouWon, showNumberAbove, showNumberBelow
- * TODO: Use the showYouWon... functions within displayResult to display the correct dialog
- * TODO: Save the guess history in a variable called guess
+ * DONE: Use the showYouWon... functions within displayResult to display the correct dialog
+ * DONE: Save the guess history in a variable called guess
  * TODO: Display the guess history using displayHistory() function
  * TODO: Use the initGame() function to restart the game
  */
 
 // Variable to store the list of guesses 
+let guesses = [];
 
 // Variable for store the correct random number 
 let correctNumber = getRandomNumber();
@@ -30,6 +31,7 @@ window.onload = function() {
 function playGame(){
   // *CODE GOES BELOW HERE *
   let numberGuess = document.getElementById('number-guess').value;
+  saveGuessHistory(numberGuess);
   displayResult(numberGuess);
   
 }
@@ -85,7 +87,8 @@ function getRandomNumber(){
  * HINT: Use the guesses variable
  */
 function saveGuessHistory(guess) {
-  // *CODE GOES BELOW HERE *
+  // *CODE GOES BELOW HERE *  
+  guesses.push(guess); 
 }
 
 /**
